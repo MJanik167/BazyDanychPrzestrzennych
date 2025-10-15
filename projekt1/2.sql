f53c30f71b40dc01fdd120fa3b32b94a102af7fd
Michał Janik <mjanik167@gmail.com>
2025-10-15 09:35:55 +0200

    done

--- projekt1/2.sql
+++ projekt1/2.sql
@@ -67,7 +67,7 @@ GROUP BY pe.stanowisko;
 SELECT pe.stanowisko,COUNT(pr.id_premii) AS liczba_premii FROM wynagrodzenie w
 JOIN pensja pe ON w.id_pensji = pe.id_pensji
 LEFT JOIN premia pr ON w.id_premii = pr.id_premii
-GROUP BY pe.stanowisko
+GROUP BY pe.stanowisko;
 
 DELETE FROM pracownicy
 WHERE id_pracownika IN (
