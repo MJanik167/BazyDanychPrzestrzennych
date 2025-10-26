b7d1dee615613b0f83efff5bdd5eb9d1f32a367f
Michał Janik <mjanik167@gmail.com>
2025-10-26 21:39:20 +0100

    project 3 final

--- projekt 3/1.sql
+++ projekt 3/1.sql
@@ -2,23 +2,24 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 
 SELECT * FROM t2019_kar_buildings b19
 JOIN t2018_kar_buildings b18 ON b18.polygon_id = b19.polygon_id
-WHERE NOT st_equals(b18.geom, b19.geom) OR b19.polygon_id NOT IN (SELECT polygon_id FROM t2018_kar_buildings);
+WHERE b19.polygon_id NOT IN (SELECT polygon_id FROM t2018_kar_buildings) OR NOT st_equals(b18.geom, b19.geom);
 
-SELECT  COUNT(*) FROM t2019_poi p2019
+SELECT  p2019.type ,COUNT(*) FROM t2019_poi p2019
 WHERE
     p2019.poi_id NOT IN (SELECT poi_id FROM t2018_poi)
     AND
     st_within(
         st_transform(p2019.geom,25832),
-        (SELECT st_union(st_buffer(ST_Transform(b19.geom, 25832),500)) FROM t2019_kar_buildings b19
+        (SELECT st_union(st_buffer(st_transform(b19.geom, 25832),500)) FROM t2019_kar_buildings b19
         JOIN t2018_kar_buildings b18 ON b18.polygon_id = b19.polygon_id
         WHERE NOT st_equals(b18.geom, b19.geom) OR b19.polygon_id NOT IN (SELECT polygon_id FROM t2018_kar_buildings))
-    );
+    )
+GROUP BY p2019.type;
 
 SELECT *, st_transform(geom,3068) as projection FROM t2019_street_node
 WHERE  st_within(
-        st_transform(st_transform(t2019_street_node.geom,3068),25832),
-        (SELECT st_buffer(ST_MakeLine(ST_Transform(geometry, 25832)),500) FROM input_points)
+        st_transform(t2019_street_node.geom,25832),
+        (SELECT st_buffer(ST_MakeLine(st_transform(geometry, 25832)),500) FROM input_points)
     );
 
 
@@ -32,9 +33,9 @@ AND
 
 
 INSERT INTO T2019_KAR_BRIDGES (geom)
-SELECT (ST_Dump(ST_Intersection(
+SELECT DISTINCT(ST_Intersection(
             ST_Transform(r.geom, 3068),
-            ST_Transform(w.geom, 3068) ))).geom
+            ST_Transform(w.geom, 3068)))
 FROM t2019_railways r
 JOIN t2019_water_lines w ON ST_Intersects(
        ST_Transform(r.geom, 3068),
