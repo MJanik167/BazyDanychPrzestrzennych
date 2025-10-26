37569deca37b0ff5ba4f670b598c7bebb00c64df
Michał Janik <mjanik167@gmail.com>
2025-10-26 14:45:15 +0100

    project 3

---
+++ projekt 3/1.sql
@@ -0,0 +1,23 @@
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+SELECT * FROM t2019_kar_buildings b19
+JOIN t2018_kar_buildings b18 ON b18.polygon_id = b19.polygon_id
+WHERE NOT st_equals(b18.geom, b19.geom) OR b19.polygon_id NOT IN (SELECT polygon_id FROM t2018_kar_buildings);
+
+SELECT  COUNT(*) FROM t2019_poi p2019
+WHERE
+    p2019.poi_id NOT IN (SELECT poi_id FROM t2018_poi)
+    AND
+    st_within(
+        st_transform(p2019.geom,25832),
+        (SELECT st_union(st_buffer(ST_Transform(b19.geom, 25832),500)) FROM t2019_kar_buildings b19
+        JOIN t2018_kar_buildings b18 ON b18.polygon_id = b19.polygon_id
+        WHERE NOT st_equals(b18.geom, b19.geom) OR b19.polygon_id NOT IN (SELECT polygon_id FROM t2018_kar_buildings))
+    );
+
+SELECT *, st_transform(geom,3068) as projection FROM t2019_street_node
+WHERE  st_within(
+        st_transform(st_transform(t2019_street_node.geom,3068),25832),
+        (SELECT st_buffer(ST_MakeLine(ST_Transform(geometry, 25832)),500) FROM input_points)
+    );
+
---
+++ projekt 3/2.sql
@@ -0,0 +1,21 @@
+CREATE TABLE input_points (
+    id SERIAL PRIMARY KEY,
+    geometry geometry(Point, 0) NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+-- INSERT INTO input_points(geometry, name) VALUES
+--                                   ('POINT(8.36093 49.03174)','1'),
+--                                   ('POINT(8.39876 49.00644)','2');
+--
+--
+-- UPDATE input_points
+-- SET geometry = ST_SetSRID(geometry, 3068);
+
+
+INSERT INTO input_points(geometry, name) VALUES
+                                    (ST_SetSRID(ST_Point(8.36093, 49.03174), 4326),'1'),
+                                    (ST_SetSRID(ST_Point(8.39876, 49.00644), 4326),'2');
+
+UPDATE input_points
+SET geometry = ST_Transform(geometry, 3068);
